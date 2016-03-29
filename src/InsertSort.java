06c7cfb63756b8750446758283b8093eaecbd0e6
liyuanhua <liyuanhua0512@126.com>
2016-03-29 22:44:16 +0800

    after fix bug,insert sort is good

--- src/InsertSort.java
+++ src/InsertSort.java
@@ -3,13 +3,15 @@
  */
 public class InsertSort {
 
+    int[] array = {3,1,2,5,4};
+
     public void sortExec(int[] array){
 
         for(int i = 1;i < array.length;i++){
-            for(int j = i;array[i] < array[j];j--){
-                int tmp = array[i];
-                array[i] = array[j];
-                array[j] = array[i];
+            for(int j = i;j > 0 && array[j] < array[j-1];j--){
+                int tmp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = tmp;
             }
 
         }
@@ -21,12 +23,12 @@ public class InsertSort {
 
     public static void main(String[] args){
 
-        QuickSort qs = new QuickSort();
+        InsertSort is = new InsertSort();
 
-        qs.sortExec(qs.array,0,qs.array.length - 1 );
+        is.sortExec(is.array);
 
-        for(int i = 0; i < qs.array.length;i++){
-            System.out.println(qs.array[i]);
+        for(int i = 0; i < is.array.length;i++){
+            System.out.println(is.array[i]);
 
         }
 
