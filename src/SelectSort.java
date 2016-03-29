1885027269752a342533eb305831e032bc6946bc
liyuanhua <liyuanhua0512@126.com>
2016-03-29 21:45:41 +0800

    first commit

---
+++ src/SelectSort.java
@@ -0,0 +1,30 @@
+/**
+ * Created by liyuanhua on 16/3/29.
+ */
+public class SelectSort {
+
+    int[] array = {3,1,2,5,4};
+
+
+    public static void sortExec(int[] array){
+
+        int k=0;
+        for(int i=1;i<array.length;i++){
+
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        QuickSort qs = new QuickSort();
+
+        qs.sortExec(qs.array, 0, qs.array.length - 1);
+
+        for(int i = 0; i < qs.array.length;i++){
+            System.out.println(qs.array[i]);
+
+        }
+
+    }
+}
