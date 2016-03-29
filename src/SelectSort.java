aab48db0116e27e092d352353dbffe7523ace6d0
liyuanhua <liyuanhua0512@126.com>
2016-03-29 22:11:05 +0800

    select sort is good

--- src/SelectSort.java
+++ src/SelectSort.java
@@ -11,7 +11,7 @@ public class SelectSort {
         for(int i = 0;i < array.length - 1;i++){
             int min = i;
             for(int j = i + 1;j < array.length;j++){
-                if(array[i] > array[j]){
+                if(array[min] > array[j]){
                     min = j;
                 }
             }
